3836836f7b46ec688594df44429e96b55edfee0e
bastien coadou <bastien.coadou@yahoo.fr>
2017-12-12 17:05:19 +0100

    exercise2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,16 @@
 exports.run = function(input) {
+  input=input.split(7).join('');
+  input=input.split(6).join('');
+  input=input.split(8).join('');
+  input=input.split(9).join('');
+  console.log
+  //for(i=0;i<input.length;i++)
+  //{
+    //if(input[i]<5)
+    //{
+     //input=input+'input[i]';
+    //}
+  //}
+  return input;
   console.log("Exercise #2");
 };
