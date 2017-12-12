51a8fd843a50198766aeaaae8862c474b2717632
bastien coadou <bastien.coadou@yahoo.fr>
2017-12-12 17:16:22 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,8 +1,12 @@
 exports.run = function(input) {
   console.log("Exercise #4");
-
   // YOUR CODE IS WELCOME HERE
-
+    var tab=[1,2,3];
+    var tab2=["one","two","Three"];
+    var myFunction= function(tab,tab2){
+      console.log("The value of"+tab2[0]+"is"+tab[0]+", The value of")
+    }
+    //non terminé
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
   /** */
