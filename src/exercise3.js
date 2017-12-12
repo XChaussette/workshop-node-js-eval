969e1a1b928a65aca788c6c5eb1fe5164f4100ad
bastien coadou <bastien.coadou@yahoo.fr>
2017-12-12 17:04:36 +0100

    exercise3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,22 @@
 exports.run = function(input) {
+  var operator={
+    "add": '+',
+      "substract":'-'
+    };
+  result=input;
+ for(i=0;i<input.length;i++)
+  {
+    if(input[i]==operator.add)
+    {
+      result=result+input[i+1];
+     console.log('operator: '+'add'+', value: '+input[i+1])
+    }
+    if(input[i]==operator.substract)
+    {
+        result=result-input[i+1]
+        console.log('operator: '+'substract'+', value: '+input[i+1])
+    }
+    return result;
+  }
   console.log("Exercise #3");
 };
